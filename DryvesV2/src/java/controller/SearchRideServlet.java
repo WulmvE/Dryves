2a2677114ef1f1ef003bbfc93549ef5c50e1441b
WulmvE <willem_vaness@hotmail.com>
2013-05-29 18:53:42 +0200

    Nou, als het goed is zijn we weer helemaal werkend!

--- DryvesV2/src/java/controller/SearchRideServlet.java
+++ DryvesV2/src/java/controller/SearchRideServlet.java
@@ -3,23 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package controller;
 
-import entity.Car;
-import entity.Dryver;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import session.CarFacade;
 import session.DryverFacade;
-import session.RideFacade;
 
 /**
  *
@@ -27,27 +21,17 @@ import session.RideFacade;
  */
 @WebServlet(name = "SearchRideServlet",
         loadOnStartup = 1,
-        urlPatterns = {"/test", "/searchRide", "/searchRideDetails", "/rideDetails", "/searchRideList", "/searchresults", "/createRide", "/createRideDetails", "/createRideConfirmed"})
+        urlPatterns = {"/test", "/searchRide", "/searchRideDetails", "/searchRideList"})
 public class SearchRideServlet extends HttpServlet {
 
     @EJB
     private DryverFacade dryverFacade;
-    @EJB
-    private RideFacade rideFacade;
-    @EJB
-    private CarFacade carFacade;
-    private String tempStartLocation;
-    private String tempEndLocation;
-    private String tempDate;
 
     @Override
     public void init() throws ServletException {
-//        getServletContext().setAttribute("dryvers", dryverFacade.findAll());
 
         // store category list in servlet context
         getServletContext().setAttribute("dryvers", dryverFacade.findAll());
-
-
     }
 
     /**
@@ -64,42 +48,11 @@ public class SearchRideServlet extends HttpServlet {
             throws ServletException, IOException {
 
         String userPath = request.getServletPath();
-        HttpSession session = request.getSession();
-        Ride selectedRide;
-//        System.out.println("GET");
-//            // if searchRideList page is requested
-//        } else if (userPath.equals("/searchRideList")) {
-//            // TODO: test, of de controller request forward naar view
-//        } else if (userPath.equals("/test")) {
-//            //dit is een test
-//        } else if (userPath.equals("/searchresults")) {
-//        }
-//        // use RequestDispatcher to forward request internally
 
         // if searchRideDetails page is requested
-        if (userPath.equals("/searchresults")) {
-//            String van = request.getParameter("search_start");
-//            String naar = request.getParameter("search_destination");
-//            String op = request.getParameter("search_date");
-//            session.setAttribute("rides", rideFacade.searchRideByStart(van));
-//            int aantalrides = rideFacade.searchRideByStart(van).size();
-//
-//
-//            session.setAttribute("aantalrides", aantalrides);
-        } else if (userPath.equals("/rideDetails")) {
-
-            String idRide = request.getQueryString();
-
-            if (idRide != null) {
-
-                // get selected ride
-                selectedRide = rideFacade.find(Integer.parseInt(idRide));
-
-                // place selected category in session scope
-                session.setAttribute("selectedRide", selectedRide);
-
-            }
-
+        if (userPath.equals("/searchRideDetails")) {
+            // TODO: Implement searchRideDetails request
+            // if searchRideList page is requested
         } else if (userPath.equals("/searchRideList")) {
             // TODO: test, of de controller request forward naar view
         } else if (userPath.equals("/test")) {
@@ -128,100 +81,22 @@ public class SearchRideServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //Search a ride.
+
         String userPath = request.getServletPath();
-        HttpSession session = request.getSession();
 
         // if searchRide action is called
-        if (userPath.equals("/searchresults")) {
-            String van = request.getParameter("search_start");
-            String naar = request.getParameter("search_destination");
-            String op = request.getParameter("search_date");
-            getServletContext().setAttribute("rides", rideFacade.searchRideByStart(van));
-
-
-            //Get the total number of Rides for this search.
-            int aantalrides = rideFacade.searchRideByStart(van).size();
-            getServletContext().setAttribute("aantalrides", aantalrides);
+        if (userPath.equals("/searchRide")) {
+            // TODO: Implement search ride action
         }
 
-        // if createRide action is called
-        if (userPath.equals("/createRide")) {
+        // use RequestDispatcher to forward request internally
+        String url = "/WEB-INF/view" + userPath + ".jsp";
 
-            // pass parameters to createRideDetails
-            tempStartLocation = request.getParameter("create_start");
-            tempEndLocation = request.getParameter("create_destination");
-            tempDate = request.getParameter("create_date");
-
-            getServletContext().setAttribute("create_start", tempStartLocation);
-            getServletContext().setAttribute("create_end", tempEndLocation);
-            getServletContext().setAttribute("create_date", tempDate);
-
-        }
-
-        // if createRideConfirmed action is called
-        if (userPath.equals("/createRideConfirmed")) {
-            //start
-            String startLocation = request.getParameter("create_start");
-            if (startLocation.equals("")) {
-                startLocation = tempStartLocation;
-            }
-            //end
-            String endLocation = request.getParameter("create_destination");
-            if (endLocation.equals("")) {
-                endLocation = tempEndLocation;
-            }
-
-            //date
-            String date = request.getParameter("create_date");
-            if (date.equals("")) {
-                date = tempDate;
-            }
-            DateFormat df = new SimpleDateFormat("mm/dd/yyyy");
-            Date dateObj = new Date();
-            try {
-                dateObj = df.parse(date);
-            } catch (ParseException ex) {
-                Logger.getLogger(SearchRideServlet.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-            //car
-             Date departureTime = new Date();
-
-            //number of seats
-            String numSeats = request.getParameter("create_num_seats");
-
-             // to be asked
-             double askingPrice = 10.0;
-             String seatsAvailable = "2";
-             boolean status = false;
-
-            Dryver dryver = new Dryver(100);
-            Car car = new Car(100);
-             Ride ride = new Ride();
-             ride.setStartLocation(startLocation);
-             ride.setEndLocation(endLocation);
-             ride.setDepartureDate(departureDate);
-             ride.setDepartureTime(departureTime);
-             ride.setAskingPrice(askingPrice);
-             ride.setSeatsAvailable(seatsAvailable);
-             ride.setStatus(status);*/
-
-            int rideId = rideFacade.placeRide(startLocation, endLocation, dryver, car, dateObj, numSeats, price);
-            if (userPath.equals("/searchRide")) {
-                // TODO: Implement search ride action
-            }
-
-
-
-            // use RequestDispatcher to forward request internally
-            String url = "/WEB-INF/view" + userPath + ".jsp";
-
-            try {
-                request.getRequestDispatcher(url).forward(request, response);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+        try {
+            request.getRequestDispatcher(url).forward(request, response);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
+
 }
